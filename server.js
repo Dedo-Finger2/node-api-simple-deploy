a637736e0fafa09942706dcb6efa0b799afc4905
Greg <antonioimportant@gmail.com>
2024-06-10 21:14:36 -0700

    feat: add routes and memory database

--- server.js
+++ server.js
@@ -2,6 +2,22 @@ import { fastify } from "fastify";
 
 const server = fastify();
 
+server.post("/videos", () => {
+  
+});
+
+server.get("/videos", () => {
+
+});
+
+server.put("/videos/:id", () => {
+
+});
+
+server.delete("/videos/:id", () => {
+
+});
+
 server.listen({ port: 3333 }, () => {
   console.log("🚀 Running...");
 });
